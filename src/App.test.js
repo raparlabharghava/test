d5345172433a933c7459755f3da9b1c32c55fa7d
raparlabharghava <brraparla@gmail.com>
2019-02-02 10:19:25 -0500

    added unit tests for action generators

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { addItemAction, removeItemAction, updateItemAction } from './actions/ItemActions.js';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+
+test('test item add action generator', () => {
+  const addAction = { type: 'addItem', item: { id: 101, name: 'laptop'} };
+  expect(addItemAction({ id: 101, name: 'laptop'})).toEqual(addAction);
+});
+
+test('test remove item action generator', () => {
+  const removeAction = { type: 'removeItem', item: { id: 101 } };
+  expect(removeItemAction({ id: 101})).toEqual(removeAction);
+});
+
+test('test item update action generator', () => {
+  const addAction = { type: 'updateItem', item: { id: 101, name: 'Desktop'} };
+  expect(updateItemAction({ id: 101, name: 'Desktop'})).toEqual(addAction);
 });
